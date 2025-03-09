d5ae33869c8c6ed5620370f8dd70285adb5b5cc7
Lucas Otávio <lucasotavioaf@gmail.com>
2025-03-09 18:20:16 -0300

    Update b.c

--- lista7/b.c
+++ lista7/b.c
@@ -6,13 +6,17 @@ void f1(int AA, int BB, int *a, int *b){
     *b=AA;
 }
 
-int main(){
+int main() {
     int AA, BB, a, b;
+   
     printf("Informe um valor A: ");
     scanf("%d", &AA);
+   
     printf("Informe um valor B: ");
     scanf("%d", &BB);
+   
     f1(AA, BB, &a, &b);
     printf("Valores A e B trocado: %d %d", a, b);
+   
     return 0;
 }
